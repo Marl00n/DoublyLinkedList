30f8af3aa4767b48e3857094b1fc2bdf6a8a950d
Jakub Piatek <kuba@MacBook-Pro-Jakub.local>
2017-10-04 20:32:04 +0200

    Initial Commit

---
+++ DoublyLinkedList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DoublyLinkedList
+//
+//  Created by Jakub Piatek on 04.10.2017.
+//  Copyright © 2017 Jakub Piatek. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
